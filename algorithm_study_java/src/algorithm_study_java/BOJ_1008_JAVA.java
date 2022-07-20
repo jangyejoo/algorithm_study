9ddb239935e35cc3f95daf587c5e0145c2e9deb3
abcdq12345 <abcdq12345@naver.com>
2022-07-21 00:31:58 +0900

    feat : BOJ_1000_1001_1008_1330_2438_JAVA

---
+++ algorithm_study_java/src/algorithm_study_java/BOJ_1008_JAVA.java
@@ -0,0 +1,22 @@
+package algorithm_study_java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1008_JAVA {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str);
+
+		double a = Integer.parseInt(st.nextToken());
+		double b = Integer.parseInt(st.nextToken());
+
+		System.out.println(a / b);
+	}
+
+}
