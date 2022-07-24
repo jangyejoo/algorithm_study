c7a4ff905450a359aa6c7428e64aaa1d59d0b13f
abcdq12345 <abcdq12345@naver.com>
2022-07-24 23:59:00 +0900

    feat : BOJ_9498_10171_10172_10809_10869_JAVA

---
+++ algorithm_study_java/src/algorithm_study_java/BOJ_10869_JAVA.java
@@ -0,0 +1,22 @@
+package algorithm_study_java;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ_10869_JAVA {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str);
+
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+
+		System.out.println(A + B);
+		System.out.println(A - B);
+		System.out.println(A * B);
+		System.out.println(A / B);
+		System.out.println(A % B);
+	}
+}
