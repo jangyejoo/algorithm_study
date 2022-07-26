f323debba85a48ed8915c586dc94c96e38f83245
abcdq12345 <abcdq12345@naver.com>
2022-07-26 22:43:51 +0900

    feat : BOJ_10950_10951_10952_10998_11654_JAVA

---
+++ algorithm_study_java/src/algorithm_study_java/BOJ_10950_JAVA.java
@@ -0,0 +1,23 @@
+package algorithm_study_java;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ_10950_JAVA {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < T; i++) {
+			String str = br.readLine();
+			StringTokenizer st = new StringTokenizer(str);
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			System.out.println(a + b);
+		}
+
+	}
+
+}
