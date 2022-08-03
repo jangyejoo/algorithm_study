a3da24fa1a8924db19e1602e6fa83673c1806120
abcdq12345 <abcdq12345@naver.com>
2022-08-03 23:52:41 +0900

    feat : solved.ac/class/1 clear!

---
+++ algorithm_study_java/src/algorithm_study_java/BOJ_2908_JAVA.java
@@ -0,0 +1,30 @@
+package algorithm_study_java;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ_2908_JAVA {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		String input = br.readLine();
+		StringTokenizer st = new StringTokenizer(input);
+		char[] A = st.nextToken().toCharArray();
+		char[] B = st.nextToken().toCharArray();
+		char[] r_A = new char[A.length];
+		char[] r_B = new char[B.length];
+		for (int i = 0; i < r_A.length; i++) {
+			r_A[i] = A[A.length - 1 - i];
+		}
+		for (int i = 0; i < r_A.length; i++) {
+			r_B[i] = B[B.length - 1 - i];
+		}
+
+		int n_A = Integer.parseInt(new String(r_A));
+		int n_B = Integer.parseInt(new String(r_B));
+		System.out.println(n_A <= n_B ? n_B : n_A);
+	}
+
+}
