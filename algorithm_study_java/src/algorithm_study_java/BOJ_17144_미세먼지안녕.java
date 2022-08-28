95a4e6ceb97ad1eaea137fa01511ad336e7562da
abcdq12345 <abcdq12345@naver.com>
2022-08-29 01:08:19 +0900

    HW : 0826

--- algorithm_study_java/src/algorithm_study_java/BOJ_17144_미세먼지안녕.java
+++ algorithm_study_java/src/algorithm_study_java/BOJ_17144_미세먼지안녕.java
@@ -1,5 +1,132 @@
 package algorithm_study_java;
 
+import java.io.*;
+import java.util.*;
+
 public class BOJ_17144_미세먼지안녕 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int R = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		int T = Integer.parseInt(st.nextToken());
+
+		int[][] map = new int[R + 1][C + 1];
+		int[][] empty = new int[R + 1][C + 1];
+
+		Queue<int[]> q = new LinkedList<>();
+		int airCleanerUp = -1;
+		int airCleanerDown = -1;
+		for (int i = 1; i <= R; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for (int j = 1; j <= C; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if (map[i][j] != -1 && map[i][j] != 0) {
+					int[] pos = { i, j };
+					q.offer(pos);
+				}
+				if (map[i][j] == -1) {
+					if (airCleanerUp != -1) {
+						airCleanerDown = i;
+					} else {
+						airCleanerUp = i;
+					}
+				}
+
+			}
+		}
+
+		int dx[] = { 1, 0, 0, -1 };
+		int dy[] = { 0, 1, -1, 0 };
+
+		while (T-- > 0) {
+			while (!q.isEmpty()) {
+				int x = q.peek()[0];
+				int y = q.peek()[1];
+				q.poll();
+				int cnt = 0;
+				for (int d = 0; d < 4; d++) {
+					int nx = x + dx[d];
+					int ny = y + dy[d];
+					if (nx < 1 || ny < 1 || nx > R || ny > C)
+						continue;
+					if ((nx == airCleanerUp || nx == airCleanerDown) && ny == 1)
+						continue;
+					cnt++;
+					empty[nx][ny] += map[x][y] / 5;
+				}
+				empty[x][y] -= map[x][y] / 5 * cnt;
+			}
+
+			for (int i = 1; i <= R; i++) {
+				for (int j = 1; j <= C; j++) {
+					if (empty[i][j] != 0) {
+						map[i][j] += empty[i][j];
+						empty[i][j] -= empty[i][j];
+					}
+				}
+			}
+
+			int x = airCleanerUp - 1;
+			int y = 1;
+			while (map[x][y] != -1) {
+				if (x == 1 && y < C) {
+					map[x][y] = map[x][y + 1];
+					y++;
+				} else if (y == 1 && x > 1) {
+					map[x][y] = map[x - 1][y];
+					x--;
+				} else if (x == airCleanerUp && y > 1) {
+					map[x][y] = map[x][y - 1] != -1 ? map[x][y - 1] : 0;
+					y--;
+				} else if (y == C && x < R) {
+					map[x][y] = map[x + 1][y];
+					x++;
+				}
+
+			}
+
+			x = airCleanerDown + 1;
+			y = 1;
+			while (map[x][y] != -1) {
+				if (x == R && y < C) {
+					map[x][y] = map[x][y + 1];
+					y++;
+				} else if (y == 1 && x < R) {
+					map[x][y] = map[x + 1][y];
+					x++;
+				} else if (x == airCleanerDown && y > 1) {
+					map[x][y] = map[x][y - 1] != -1 ? map[x][y - 1] : 0;
+					y--;
+				} else if (y == C && x > 1) {
+					map[x][y] = map[x - 1][y];
+					x--;
+				}
+
+			}
+
+			for (int i = 1; i <= R; i++) {
+				for (int j = 1; j <= C; j++) {
+					if (map[i][j] != -1 && map[i][j] != 0) {
+						int[] pos = { i, j };
+						q.offer(pos);
+					}
+				}
+			}
+		}
+
+		int result = 0;
+		for (int i = 1; i <= R; i++) {
+			for (int j = 1; j <= C; j++) {
+				if (map[i][j] != -1)
+					result += map[i][j];
+			}
+		}
+
+		System.out.println(result);
+
+	}
 
 }
---
+++ algorithm_study_java/src/algorithm_study_java/SWEA_4013_특이한자석.java
@@ -0,0 +1,191 @@
+package algorithm_study_java;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class SWEA_4013_특이한자석 {
+
+	static int[] m1 = new int[9];
+	static int[] m2 = new int[9];
+	static int[] m3 = new int[9];
+	static int[] m4 = new int[9];
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		int t = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= t; tc++) {
+			int k = Integer.parseInt(br.readLine());
+
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+			for (int i = 1; i <= 8; i++) {
+				m1[i] = Integer.parseInt(st.nextToken());
+			}
+
+			st = new StringTokenizer(br.readLine(), " ");
+
+			for (int i = 1; i <= 8; i++) {
+				m2[i] = Integer.parseInt(st.nextToken());
+			}
+
+			st = new StringTokenizer(br.readLine(), " ");
+
+			for (int i = 1; i <= 8; i++) {
+				m3[i] = Integer.parseInt(st.nextToken());
+			}
+
+			st = new StringTokenizer(br.readLine(), " ");
+
+			for (int i = 1; i <= 8; i++) {
+				m4[i] = Integer.parseInt(st.nextToken());
+			}
+
+			for (int i = 0; i < k; i++) {
+				st = new StringTokenizer(br.readLine(), " ");
+				int mNum = Integer.parseInt(st.nextToken());
+				int direction = Integer.parseInt(st.nextToken());
+
+				switch (mNum) {
+				case 1:
+//					1번 자석 돌리기
+					if (m1[3] != m2[7]) {
+						if (m2[3] != m3[7]) {
+							if (m3[3] != m4[7]) {
+								rotate4(direction * -1);
+							}
+							rotate3(direction);
+						}
+						rotate2(direction * -1);
+					}
+					rotate1(direction);
+					break;
+				case 2:
+//					2번 자석 돌리기
+					if (m2[3] != m3[7]) {
+						if (m3[3] != m4[7]) {
+							rotate4(direction);
+						}
+						rotate3(direction * -1);
+					}
+					if (m2[7] != m1[3]) {
+						rotate1(direction * -1);
+					}
+					rotate2(direction);
+					break;
+				case 3:
+//					3번 자석 돌리기
+					if (m3[3] != m4[7]) {
+						rotate4(direction * -1);
+					}
+					if (m3[7] != m2[3]) {
+						if (m2[7] != m1[3]) {
+							rotate1(direction);
+						}
+						rotate2(direction * -1);
+					}
+					rotate3(direction);
+					break;
+				case 4:
+//					4번 자석 돌리기
+					if (m4[7] != m3[3]) {
+						if (m3[7] != m2[3]) {
+							if (m2[7] != m1[3]) {
+								rotate1(direction * -1);
+							}
+							rotate2(direction);
+						}
+						rotate3(direction * -1);
+					}
+					rotate4(direction);
+					break;
+
+				}
+			}
+
+			int result = 0;
+			if (m1[1] == 1)
+				result += 1;
+			if (m2[1] == 1)
+				result += 2;
+			if (m3[1] == 1)
+				result += 4;
+			if (m4[1] == 1)
+				result += 8;
+			sb.append("#").append(tc).append(" ").append(result).append("\n");
+
+		}
+
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+
+	}
+
+	private static void rotate4(int d) {
+		if (d == 1) {
+			int tmp = m4[8];
+			for (int i = 7; i >= 1; i--) {
+				m4[i + 1] = m4[i];
+			}
+			m4[1] = tmp;
+		} else {
+			int tmp = m4[1];
+			for (int i = 2; i <= 8; i++) {
+				m4[i - 1] = m4[i];
+			}
+			m4[8] = tmp;
+		}
+	}
+
+	private static void rotate3(int d) {
+		if (d == 1) {
+			int tmp = m3[8];
+			for (int i = 7; i >= 1; i--) {
+				m3[i + 1] = m3[i];
+			}
+			m3[1] = tmp;
+		} else {
+			int tmp = m3[1];
+			for (int i = 2; i <= 8; i++) {
+				m3[i - 1] = m3[i];
+			}
+			m3[8] = tmp;
+		}
+	}
+
+	private static void rotate2(int d) {
+		if (d == 1) {
+			int tmp = m2[8];
+			for (int i = 7; i >= 1; i--) {
+				m2[i + 1] = m2[i];
+			}
+			m2[1] = tmp;
+		} else {
+			int tmp = m2[1];
+			for (int i = 2; i <= 8; i++) {
+				m2[i - 1] = m2[i];
+			}
+			m2[8] = tmp;
+		}
+	}
+
+	private static void rotate1(int d) {
+		if (d == 1) {
+			int tmp = m1[8];
+			for (int i = 7; i >= 1; i--) {
+				m1[i + 1] = m1[i];
+			}
+			m1[1] = tmp;
+		} else {
+			int tmp = m1[1];
+			for (int i = 2; i <= 8; i++) {
+				m1[i - 1] = m1[i];
+			}
+			m1[8] = tmp;
+		}
+	}
+
+}
