c866bb81e3f210ac309b58e4725ffae3db45a482
abcdq12345 <abcdq12345@naver.com>
2022-10-17 12:28:30 +0900

    feat : add_boj_1018, boj_2108, swea_5607

---
+++ algorithm_study_java/src/BOJ/BOJ_1018_체스판다시칠하기.java
@@ -0,0 +1,69 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1018_체스판다시칠하기 {
+
+	static char[][] blackboard = { { 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B' },
+			{ 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W' }, { 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B' },
+			{ 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W' }, { 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B' },
+			{ 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W' }, { 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B' },
+			{ 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W' }, { 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B' },
+			{ 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W' } };
+
+	static char[][] whiteboard = { { 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W' },
+			{ 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B' }, { 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W' },
+			{ 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B' }, { 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W' },
+			{ 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B' }, { 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W' },
+			{ 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B' }, { 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W' },
+			{ 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B' } };
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		char[][] board = new char[n][m];
+		for (int i = 0; i < n; i++) {
+			char[] row = br.readLine().toCharArray();
+			for (int j = 0; j < m; j++) {
+				board[i][j] = row[j];
+			}
+		}
+
+//		init done
+
+		int ans = Integer.MAX_VALUE;
+
+		for (int i = 0; i <= n - 8; i++) {
+			for (int j = 0; j <= m - 8; j++) {
+				int result = calc(board, i, j);
+				if (ans > result)
+					ans = result;
+			}
+		}
+		System.out.println(ans);
+	}
+
+	private static int calc(char[][] board, int startH, int startW) {
+		int cntB = 0;
+		int cntW = 0;
+		int idxH = 0;
+		for (int i = startH; i < startH + 8; i++) {
+			int idxW = 0;
+			for (int j = startW; j < startW + 8; j++) {
+				if (board[i][j] != blackboard[idxH][idxW])
+					cntB++;
+				if (board[i][j] != whiteboard[idxH][idxW])
+					cntW++;
+				idxW++;
+			}
+			idxH++;
+		}
+		return Math.min(cntB, cntW);
+	}
+
+}
