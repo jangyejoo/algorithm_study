d909c8f4bdf5c177aafd30a6e475bd5b6660409e
abcdq12345 <abcdq12345@naver.com>
2023-07-17 16:33:46 +0900

    feat : add_boj_20922_겹치는건싫어

---
+++ algorithm_study_java/src/BOJ/BOJ_20922_겹치는건싫어.java
@@ -0,0 +1,45 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_20922_겹치는건싫어 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine().trim());
+
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+
+		int[] input = new int[n];
+		st = new StringTokenizer(br.readLine().trim());
+		for (int i = 0; i < n; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+		}
+
+		// init done
+
+		int max = 0;
+		int start = 0;
+		int end = 0;
+		int[] cnt = new int[100001];
+		while (start < n) {
+			while (end < n) {
+				if (cnt[input[end]] + 1 > k) {
+					break;
+				} else {
+					cnt[input[end]]++;
+					end++;
+				}
+			}
+			max = Math.max(max, end - start);
+			cnt[input[start]]--;
+			start++;
+		}
+		System.out.println(max);
+	}
+
+}
