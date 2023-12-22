083a365d4d5042bdae9fbd8f9cc0f3860de0b5ff
abcdq12345 <abcdq12345@naver.com>
2023-12-22 10:45:53 +0900

    feat : add_boj_2981_검문

---
+++ algorithm_study_java/src/BOJ/BOJ_2981_검문.java
@@ -0,0 +1,51 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class BOJ_2981_검문 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		int n = Integer.parseInt(br.readLine());
+		int[] input = new int[n];
+		for (int i = 0; i < n; i++) {
+			input[i] = Integer.parseInt(br.readLine());
+		}
+
+		// init done
+
+		// 정렬
+		Arrays.sort(input);
+
+		int min = Integer.MAX_VALUE;
+		int[] gap = new int[n - 1];
+		for (int i = 0; i < n - 1; i++) {
+			gap[i] = Math.abs(input[i] - input[i + 1]);
+			min = Math.min(min, gap[i]);
+		}
+
+		// gap들의 공약수 구하기
+		C: for (int i = 2; i <= min; i++) {
+			for (int j = 0; j < n - 1; j++) {
+				if (gap[j] % i != 0) {
+					continue C;
+				}
+			}
+			sb.append(i).append(" ");
+		}
+
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+
+	}
+
+}
