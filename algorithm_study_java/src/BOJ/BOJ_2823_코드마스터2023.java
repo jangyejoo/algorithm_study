782df095d8500bb3821d863c95f40c1a656ffc58
abcdq12345 <abcdq12345@naver.com>
2024-01-10 16:54:53 +0900

    feat : add_boj_2823_코드마스터2023

---
+++ algorithm_study_java/src/BOJ/BOJ_2823_코드마스터2023.java
@@ -0,0 +1,29 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2823_코드마스터2023 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String input = br.readLine();
+		switch (input) {
+		case "SONGDO":
+			System.out.println("HIGHSCHOOL");
+			break;
+		case "CODE":
+			System.out.println("MASTER");
+			break;
+		case "2023":
+			System.out.println("0611");
+			break;
+		case "ALGORITHM":
+			System.out.println("CONTEST");
+			break;
+		}
+	}
+
+}
