fcc38da53586c3de33b935e0be6d05ba8a2ab930
abcdq12345 <abcdq12345@naver.com>
2024-01-10 17:15:52 +0900

    feat : add_boj_15964_이상한기호

---
+++ algorithm_study_java/src/BOJ/BOJ_15964_이상한기호.java
@@ -0,0 +1,20 @@
+package BOJ;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_15964_이상한기호 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine().trim());
+		long a = Long.parseLong(st.nextToken());
+		long b = Long.parseLong(st.nextToken());
+
+		System.out.println((a + b) * (a - b));
+	}
+
+}
