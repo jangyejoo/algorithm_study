c11ae49054dcb8f3f7351fbb371aeb52697f0035
abcdq12345 <abcdq12345@naver.com>
2024-01-10 17:06:08 +0900

    feat : add_boj_2377_Pottery

---
+++ algorithm_study_java/src/BOJ/BOJ_2377_Pottery.java
@@ -0,0 +1,11 @@
+package BOJ;
+
+public class BOJ_2377_Pottery {
+
+	public static void main(String[] args) {
+		System.out.println(".  .   .");
+		System.out.println("|  | _ | _. _ ._ _  _");
+		System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+	}
+
+}
